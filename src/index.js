1be148ebf7f406f0efc7971027562600fbc75d65
wangteng <wangteng@inke.cn>
2019-10-29 14:17:58 +0800

    修改readme

--- src/index.js
+++ src/index.js
@@ -5,11 +5,15 @@ import Demo1 from './pages/demo1'
 // import Demo3 from './pages/demo3'
 import { Provider } from 'react-redux'
 // import store from './pages/demo1/store'
-import store from './pages/demo3/store'
+// import store from './pages/demo3/store'
 //声明一个App组件，然后这个组件用Provider进行包裹。
-// ReactDOM.render(<Provider store={store}>
-//     <Demo3 />
-// </Provider>, document.getElementById('root'));
+
 ReactDOM.render(
     <Demo1 />
 , document.getElementById('root'));
+// ReactDOM.render(
+//     <Demo2 />
+// , document.getElementById('root'));
+// ReactDOM.render(<Provider store={store}>
+//     <Demo3 />
+// </Provider>, document.getElementById('root'));
